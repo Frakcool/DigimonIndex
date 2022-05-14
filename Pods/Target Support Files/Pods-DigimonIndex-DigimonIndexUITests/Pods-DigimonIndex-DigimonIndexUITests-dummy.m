e411d92514cb8e96292e085f6abd46a7006d5173
Frakcool <frakcool1993@gmail.com>
2022-05-13 23:20:31 -0500

    Removed Pods folder added by mistake

--- Pods/Target Support Files/Pods-DigimonIndex-DigimonIndexUITests/Pods-DigimonIndex-DigimonIndexUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigimonIndex_DigimonIndexUITests : NSObject
-@end
-@implementation PodsDummy_Pods_DigimonIndex_DigimonIndexUITests
-@end
--- Pods/Target Support Files/Pods-DigimonIndexTests/Pods-DigimonIndexTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigimonIndexTests : NSObject
-@end
-@implementation PodsDummy_Pods_DigimonIndexTests
-@end
